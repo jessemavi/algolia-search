6796b4597218190f5643f05b77f4436d3df9fe09
Jesse Mavi <jesse.mavi@gmail.com>
2018-10-05 18:59:04 -0500

    Adds post request to add an app to the appstore index

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 const api_keys = require('./api_keys');
 const algoliasearch = require('algoliasearch');
-// move ADMIN API Key to separate file
-const client = algoliasearch('0Z8CJDE7SH', api_keys.keys.algolia_search);
+const client = algoliasearch('0Z8CJDE7SH', api_keys.algolia_search);
 const index = client.initIndex('appstore_search');
 
-app.get('/', (req, res) => {
-  res.send('main route');
-});
+app.use(bodyParser.json());
 
 // Add an app (as a JSON object) to the Algolia apps index and return its id
 app.post('/api/1/apps', (req, res) => {
+  const app = {
+    name: req.body.name,
+    image: req.body.image,
+    link: req.body.link,
+    category: req.body.category,
+    rank: req.body.rank
+  };
 
+  const add_app_to_index = async () => {
+    try {
+      const added_app = await index.addObject(app);
+      res.send(added_app.objectID);
+    } catch(err) {
+      res.send(err);
+    }
+  };
+
+  add_app_to_index();
 });
 
 // Delete an app from the Algolia index
