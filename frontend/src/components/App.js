9dfe642d5809722101dbe1c9904b3971bdb8a5a3
Jesse Mavi <jesse.mavi@gmail.com>
2018-10-07 19:57:15 -0500

    Adds algolia search call to appstore_search index in frontend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,29 @@
 import { version, Component } from 'inferno';
 import '../registerServiceWorker';
+import algoliasearch from 'algoliasearch';
+import algoliasearchHelper from 'algoliasearch-helper';
+import keys from '../api_keys';
+
+const client = algoliasearch('0Z8CJDE7SH', keys.algolia_search);
+const helper = algoliasearchHelper(client, 'appstore_search');
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hits: []
+    };
+
+    helper.on('result', async (content) => {
+      await this.setState({
+        hits: content.hits
+      });
+      console.log(this.state);
+    });
+
+    helper.search();
+  }
+
   render() {
     return (
       <div className="App">
@@ -16,7 +38,7 @@ export default App;
 
 
 // components:
-  // search box
-  // list of apps found (hits)
-    // a way to sort the results by rank ASC or rank DESC (default)
-  // categories filtering (faceting)
+  // main app: keeps track of state and makes requests to algolia, passes results to apps list(functional stateless) and app categories to categories
+    // search box: passes search input to app
+    // list of apps found (hits) with a way to sort the results by rank ASC or rank DESC (default)
+    // categories filtering (faceting)
